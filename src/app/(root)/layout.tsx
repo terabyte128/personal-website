1f425937a247e5a84bf1318f9a6b0f7e7bd7ee3c
Sam Wolfson <terabyte128@gmail.com>
2025-10-17 23:36:10 -0700

    the suspense is killing me

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import { Navigation } from "./_navigation";
 
 export default function RootLayout({
@@ -9,7 +11,7 @@ export default function RootLayout({
     <div>
       <Navigation />
       <div className="main-body mx-auto mt-20 mb-12 max-w-4xl px-4 md:px-0">
-        {children}
+        <Suspense>{children}</Suspense>
       </div>
     </div>
   );
